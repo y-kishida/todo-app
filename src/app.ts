0e565c2f083d457593cf1f8ff04250ac92a0b552
y-kishida <k.yoshikazu1221@gmail.com>
2021-02-13 19:42:27 +0900

    enum filtering - プロジェクト作成時、重複したものを表示させないように変更 - 実行中と完了中共に表示させないように変更

--- src/app.ts
+++ src/app.ts
@@ -129,7 +129,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === 'active') {
+         return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.finished;
+      })
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     })
     this.attach();
@@ -140,6 +146,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`,
     )! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
